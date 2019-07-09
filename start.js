533c24e25df9f78ffcaa5b5b9d86ecec27e9f299
Gonzalo Lamatta <gonzalo.lamatta@cencosud.cl>
2019-07-08 23:49:13 -0400

    Se agrega DB versión inicial

--- start.js
+++ start.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const {Firestore} = require('@google-cloud/firestore');
 const config = require('./config.json');
 const identifier = require('./controllers/mutanIdentifier');
 const data = require('./test/dna');
 
+const firestore = new Firestore();
+const db = new Firestore({
+  projectId: 'mutants-246102',
+  keyFilename: 'C:\\DEV\\keys\\mutants-4f181f170813.json',
+});
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/mutant', (req, res) => {
 	let dna = req.body.dna;
+	let addedDna = db.collection('dna').add({
+	  addedAt: new Date(),
+	  data: JSON.stringify(req.body)
+	}).then(ref => {
+	  console.log('Added document with ID: ', ref.id);
+	});
 	let count = 0;
 	count += identifier.findHorizontalMatch(dna);
 	count += identifier.findVerticalMatch(dna);
@@ -21,4 +36,11 @@ app.post('/mutant', (req, res) => {
 	}
 });
 
-app.listen(config.port, () => console.log(`app listening on port ${config.port}!`));
+app.get('/stats', (req, res) => {
+	res.send( JSON.stringify({count_mutant_dna: 40, count_human_dna: 100, ratio: 0.4}) );
+});
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
