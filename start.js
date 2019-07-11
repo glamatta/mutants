d38effd17fb3c42d8c142efecd76cd7d16d99a39
Gonzalo Lamatta <gonzalo.lamatta@cencosud.cl>
2019-07-10 22:10:56 -0400

    se agrega método de /stats y mejoras en varias cosas

--- start.js
+++ start.js
@@ -6,10 +6,7 @@ const config = require('./config.json');
 const identifier = require('./controllers/mutanIdentifier');
 const data = require('./test/dna');
 
-const db = new Firestore({
-  projectId: 'mutants-246102',
-  keyFilename: 'C:\\DEV\\keys\\mutants-4f181f170813.json',
-});
+const db = new Firestore(config.dbConnection);
 
 const dnaCollection = db.collection('dna');
 
@@ -18,33 +15,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/mutant', (req, res) => {
 	let dna = req.body.dna;
-	dnaCollection.where('data', '==', JSON.stringify(req.body)).get().then(
-		snapshot => {
-			if (snapshot.empty) {
-				dnaCollection.add({
-				  addedAt: new Date(),
-				  data: JSON.stringify(req.body)
-				});
-			} else { console.info("ya estaba!");}
-		});
 	let count = 0;
 	count += identifier.findHorizontalMatch(dna);
 	count += identifier.findVerticalMatch(dna);
 	count += identifier.findDiagonalRightMatch(dna);
 	count += identifier.findDiagonalLeftMatch(dna);
 
-	if (count >= config.minMatchesQuantity) {
-		res.status(200).send({"isMutant": true, "message": count + " matches"});
-	} else{
-		res.status(403).send({"isMutant": false, "message": "" + count + " matches"});
-	}
+	let isMutant = count >= config.minMatchesQuantity ? true : false;
+	let statusCode = isMutant ? 200 : 403;
+	res.status(statusCode).json({"isMutant": isMutant, "message": count + " matches"});
+
+	let dnaItem = {
+	  addedAt: new Date(),
+	  data: JSON.stringify(req.body),
+	  isMutant: isMutant
+	};
+
+	dnaCollection.where('data', '==', dnaItem.data).get().then(
+		snapshot => { if (snapshot.empty) dnaCollection.add(dnaItem) }
+	);
 });
 
 app.get('/stats', (req, res) => {
-	res.send( JSON.stringify({count_mutant_dna: 40, count_human_dna: 100, ratio: 0.4}) );
+	let countMutant = 0, countHuman = 0;
+
+	dnaCollection.where('isMutant', '==', true).get()
+	.then(snap => countMutant = snap.size)
+	.then(() => {
+		dnaCollection.where('isMutant', '==', false).get()
+		.then(snap => countHuman = snap.size)
+		.then(() => {
+			res.json({
+				count_mutant_dna: countMutant, 
+				count_human_dna: countHuman, 
+				ratio: (countMutant / (countMutant + countHuman))
+			});
+		}).catch(error => console.error("db error: ", error));
+	}).catch(error => console.error("db error: ", error));
 });
 
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+const PORT = process.env.PORT || config.defaultPort;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
